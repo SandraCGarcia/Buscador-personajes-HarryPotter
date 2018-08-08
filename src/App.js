281df5a314bd608d2f86cc05afd9e4f784e44166
SandraCGarcia <sandra.gcanete@gmail.com>
2018-08-08 14:34:24 +0200

    start route

--- src/App.js
+++ src/App.js
@@ -11,10 +11,8 @@ class App extends Component {
     this.getharacter = this.getCharacter.bind(this);
   }
 
-
   getCharacter() {
     fetch("http://hp-api.herokuapp.com/api/characters")
-
       .then(data => {
         return data.json();
       })
@@ -25,44 +23,48 @@ class App extends Component {
       });
   }
 
-  filterSearch = (e) => {
+  filterSearch = e => {
     const character = e.currentTarget.value;
-  
+
     this.setState({
       filterSearch: character
-    })
-  }
+    });
+  };
 
   render() {
-
     this.getCharacter();
 
     return (
       <div className="App">
         <h1 className="title">Harry Potter Characters</h1>
-        <input type="text" name="personaje" onChange={this.filterSearch}></input>
+        <input type="text" name="personaje" onChange={this.filterSearch} />
         <ul className="card__container">
           {this.state.datacharacter
-          
-          
-          .filter(filtro =>{
-            return filtro.name.toLowerCase().includes(this.state.filterSearch.toLowerCase())
-          })
-          .map(data => {
-            return (
-              <div className="card">
-                <li className="card__list">
-                <div className="card__list--photo">
-                  <img className="card__photo" src={data.image} alt={data.name}/>
-                  </div>
-                  <div className="card__info">
-                  <h2 className="card__name">{data.name}</h2>
-                  <p className="card__house">{data.house}</p>
+
+            .filter(filtro => {
+              return filtro.name
+                .toLowerCase()
+                .includes(this.state.filterSearch.toLowerCase());
+            })
+            .map(data => {
+              return (
+                <div className="card">
+                  <li className="card__list">
+                    <div className="card__list--photo">
+                      <img
+                        className="card__photo"
+                        src={data.image}
+                        alt={data.name}
+                      />
+                    </div>
+                    <div className="card__info">
+                      <h2 className="card__name">{data.name}</h2>
+                      <p className="card__house">{data.house}</p>
+                    </div>
+                  </li>
                 </div>
-                </li>
-              </div>
-            );
-          })}
+              );
+            })}
         </ul>
       </div>
     );
