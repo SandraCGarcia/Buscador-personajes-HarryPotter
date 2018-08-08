cfc7648967b51073aa285daca6089f3c1b8ae53b
SandraCGarcia <sandra.gcanete@gmail.com>
2018-08-08 10:01:41 +0200

    devuelve array

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datacharacter: []
+    };
+    this.getharacter = this.getCharacter.bind(this);
+  }
+  getCharacter() {
+    fetch("http://hp-api.herokuapp.com/api/characters")
+
+      .then(data => {
+        return data.json();
+      })
+      .then(data => {
+        this.setState({
+          datacharacter: data
+        });
+      });
+  }
+
   render() {
+
+    this.getCharacter();
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="title">Harry Potter Characters</h1>
+        <ul className="grid">
+          {this.state.datacharacter.map(data => {
+            return (
+              <div className="card">
+                <li className="card__list">
+                  <img className="card__photo" src={data.image} alt={data.name}/>
+                  <h2 className="card__name">{data.name}</h2>
+                  <p className="card__house">{data.house}</p>
+                </li>
+              </div>
+            );
+          })}
+          }
+        </ul>
       </div>
     );
   }
