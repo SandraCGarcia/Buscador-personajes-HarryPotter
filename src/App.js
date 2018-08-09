2b1839b47b088ed486a6c8be5ab94418f275dbe4
SandraCGarcia <sandra.gcanete@gmail.com>
2018-08-09 12:31:09 +0200

    falta maquetar detalles personaje

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <h1 className="title">Harry Potter Characters</h1>
         <main>
           <Switch>
             <Route
--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -1,31 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/moreinfo.css";
+
 
 class MoreInfo extends React.Component {
+ 
+
+vivo() {
+  const indice = this.props.match.params.id;
+  const dataVivo = this.props.characters[indice];
+  if (dataVivo.alive===true){
+    return <p>vivo</p>;
+  }else{
+return <p>muerto</p>
+  }
+}
+
 
   render() {
     const id = this.props.match.params.id;
     const data = this.props.characters[id];
 
     return (
-      <div>
-        <li>
-          <div>
-            <img src={data.image} alt={data.name} />
+      <React.Fragment>
+      <div className="info__container">
+          <div className="info__photo" style={ {backgroundImage: `url(${data.image})`} }>
+            <img className="info__photo--imagen" src={data.image} alt={data.name} />
           </div>
-          <div>
-            <h2>{data.name}</h2>
-            <p>{data.house}</p>
-            <p>{data.dateOfBirth}</p>
-            <p>{data.patronus}</p>
+          <div className="info__content">
+          <div className="info__content--title">
+            <h2 className="info__titlename">{data.name}</h2>
+            </div>
+            <div className="info__content--title">
+            <p className="info__text">{data.house}</p>
+            <p className="info__text">{data.dateOfBirth}</p>
+            <p className="info__text">{data.patronus}</p>
+            {this.vivo()}
           </div>
-        </li>
+          </div>
+          </div>
         <div>
         <Link className="card__link" to='/'>
-          <p>Return</p>
+          <p className="info__return">Return</p>
           </Link>
         </div>
-      </div>
+      </React.Fragment>
 
       // <div>{id}-{this.props.characters[id].name}</div>
 
